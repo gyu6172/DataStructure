bfb6f2cdda8cba8b872eccf778c184d4d333bad6
gyu6172 <gyu6172@gmail.com>
2023-03-20 18:11:26 +0900

    0320-재귀 코드 수정

--- 자료구조/0320-문제4.cpp
+++ 자료구조/0320-문제4.cpp
@@ -1,12 +1,7 @@
 #include <stdio.h>
 int max(int n, int arr[]) {
 	if (n == 0) {
-		if (arr[n] < arr[n + 1]) {
-			return arr[n+1];
-		}
-		else {
-			return arr[n];
-		}
+		return arr[n];
 	}
 	else if (max(n - 1, arr) < arr[n-1]) {
 		return arr[n-1];
